b3d6e07e50ca89f5b2a6db7661294dafde2a6f9a
Juan Zora <camilozora@hotmail.com>
2023-02-01 19:04:51 -0500

    Insercion de Datos BS

--- JuanZora_BS_script.sql
+++ JuanZora_BS_script.sql
@@ -8,47 +8,54 @@ created_at TIMESTAMP,
 modified_at TIMESTAMP,
 deleted_at TIMESTAMP,
 PRIMARY KEY (id)
-);
+)AUTO_INCREMENT=101;
+SELECT * FROM product_inventory;
 
 CREATE TABLE product_category (
-id INT NOT NULL AUTO_INCREMENT,
+id INT NOT NULL,
 category_name VARCHAR(45) NOT NULL,
-descriptn TEXT,
+descriptn VARCHAR(255),
 created_at TIMESTAMP,
 modified_at TIMESTAMP,
 deleted_at TIMESTAMP,
 PRIMARY KEY (id)
 );
+SELECT * FROM product_category;
 
-CREATE TABLE discount (
+
+CREATE TABLE product_category_discount (
 id INT NOT NULL AUTO_INCREMENT,
-discount_name VARCHAR(45) NOT NULL,
-descriptn VARCHAR(250),
-discount_percent DECIMAL(2,2),
+discount_name VARCHAR(65) NOT NULL ,
+descriptn VARCHAR(255),
+product_category_id INT,
+discount_percent DECIMAL(4,2),
 is_active BOOLEAN NOT NULL,
 created_at TIMESTAMP,
+valid_until TIMESTAMP,
 modified_at TIMESTAMP,
 deleted_at TIMESTAMP,
-PRIMARY KEY (id)
+PRIMARY KEY (id),
+FOREIGN KEY (product_category_id) REFERENCES product_category (id) ON DELETE SET NULL
 );
+SELECT * FROM product_category_discount;
+
 
 CREATE TABLE product (
 id INT NOT NULL AUTO_INCREMENT,
-prod_name VARCHAR(45) NOT NULL,
-descriptn TEXT,
+prod_name VARCHAR(65) NOT NULL,
+descriptn VARCHAR(255),
+model_year CHAR(4),
 price DECIMAL(10,2) NOT NULL,
 category_id INT,
 inventory_id INT,
-discount_id INT,
 created_at TIMESTAMP NOT NULL,
 modified_at TIMESTAMP NOT NULL,
-deleted_at TIMESTAMP NOT NULL,
+deleted_at TIMESTAMP,
 PRIMARY KEY (id),
 FOREIGN KEY (category_id) REFERENCES product_category (id) ON DELETE SET NULL,
-FOREIGN KEY (inventory_id) REFERENCES product_inventory (id) ON DELETE SET NULL,
-FOREIGN KEY (discount_id) REFERENCES discount (id) ON DELETE SET NULL 
+FOREIGN KEY (inventory_id) REFERENCES product_inventory (id) ON DELETE SET NULL
 );
-
+SELECT * FROM product;
 /*--TABLAS DE PRODUCTOS--*/
 
 /*--TABLAS DE CLIENTES--*/
@@ -63,6 +70,7 @@ created_at TIMESTAMP,
 modified_at TIMESTAMP,
 PRIMARY KEY (id)
 );
+SELECT * FROM users;
 
 CREATE TABLE user_address (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -73,6 +81,7 @@ country VARCHAR(45),
 mobile VARCHAR(45),
 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+SELECT * FROM user_address;
 
 CREATE TABLE user_payment (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -83,6 +92,8 @@ account_no VARCHAR(45),
 expiry DATE,
 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+SELECT * FROM user_payment;
+
 /*--TABLAS DE CLIENTES--*/
 
 /*--TABLAS DE VENTAS--*/
@@ -95,6 +106,7 @@ payment_status VARCHAR(45),
 created_at TIMESTAMP,
 modified_at TIMESTAMP
 );
+SELECT * FROM payment_details;
 
 CREATE TABLE order_details (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -106,6 +118,7 @@ modified_at TIMESTAMP,
 FOREIGN KEY (user_id) REFERENCES users (id),
 FOREIGN KEY (payment_id) REFERENCES payment_details (id)
 );
+SELECT * FROM order_details;
 
 CREATE TABLE order_items (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -117,6 +130,7 @@ modified_at TIMESTAMP,
 FOREIGN KEY (order_id) REFERENCES order_details (id),
 FOREIGN KEY (product_id) REFERENCES product (id)
 );
+SELECT * FROM order_items;
 
 
 
@@ -128,6 +142,7 @@ created_at TIMESTAMP,
 modified_at TIMESTAMP,
 FOREIGN KEY (user_id) REFERENCES users (id)
 ); 
+SELECT * FROM shopping_session;
 
 CREATE TABLE cart_item (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -139,6 +154,7 @@ modified_at TIMESTAMP,
 FOREIGN KEY (session_id) REFERENCES shopping_session (id),
 FOREIGN KEY (product_id) REFERENCES product (id)
 );
+SELECT * FROM cart_item;
 /*--TABLAS DE VENTAS--*/
 
 
